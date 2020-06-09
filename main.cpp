a7c2438d0690820aeb66c03d30d465163d2140d1
Jilin Huang <jilin.huang@gmail.com>
2020-06-08 23:32:54 -0500

    add log for program

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main()
   [out = std::ref(std::cout << "Hello ")] () {
       out.get() << "World\n";
       out.get() << typeid(out).name();
+      out.get() << "lambda with variable";
   } ();
   return 0;
 }
